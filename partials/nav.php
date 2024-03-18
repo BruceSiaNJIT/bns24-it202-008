90554c07fddcb367267c0b840bddbb216410ff80
BruceSiaNJIT <bns24@njit.edu>
2024-03-18 14:09:38 -0400

    Added more functions and fleshed it out, fixed CSS

--- partials/nav.php
+++ partials/nav.php
@@ -38,3 +38,6 @@ require_once(__DIR__ . "/../lib/functions.php");
         <?php endif; ?>
     </ul>
 </nav>
+
+<script src = "helpers.js"></script>
+<link rel = "stylesheet" href = "styles.css"></link>
